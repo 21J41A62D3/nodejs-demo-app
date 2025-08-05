c6fdd9379703a73ec8c79295173823dd6eb75df8
Prasanna <arramsailaxmi727@gmail.com>
2025-08-05 07:23:51 +0530

    Fix: correct app.js with single res.end

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
+const PORT = 3000;
+
 const server = http.createServer((req, res) => {
   res.end('Hello from Node.js App running in Docker!');
 });
 
-server.listen(3000, () => {
-  console.log('Server started on port 3000');
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
