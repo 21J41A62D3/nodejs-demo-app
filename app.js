6d3866b1ab1ce4aa273fcbb49706fb97d4250528
Prasanna <arramsailaxmi727@gmail.com>
2025-08-05 05:55:55 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-// app.js
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Node.js App running in Docker!");
+  res.end("Hello from GitHub Actions + Docker! 🚀");
 });
 
 server.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server running on http://localhost:3000");
 });
